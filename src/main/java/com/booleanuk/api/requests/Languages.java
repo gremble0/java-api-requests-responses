956af9a9de97f6c7dc2cbf6751e8f95804c99aa5
gremble0 <hermanstornes@gmail.com>
2024-08-28 10:18:47 +0200

    Add delete endpoint

--- src/main/java/com/booleanuk/api/requests/Languages.java
+++ src/main/java/com/booleanuk/api/requests/Languages.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,6 +42,15 @@ public class Languages {
           this.languages.remove(oldLanguage);
           this.languages.add(newLanguage);
           return newLanguage;
+        });
+  }
+
+  @DeleteMapping("/{name}")
+  public Optional<Language> deleteByName(@PathVariable String name) {
+    return this.getByName(name)
+        .map(languageToDelete -> {
+          this.languages.remove(languageToDelete);
+          return languageToDelete;
         });
   }
 }
